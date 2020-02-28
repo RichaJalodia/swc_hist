ca74ab2ea25b7cc612b37a3e1d696ef813bad99c
Richa Jalodia <rjalodia@med.miami.edu>
2020-02-28 10:53:42 -0500

    revert to previous version, removed extra claims

--- hist.py
+++ hist.py
@@ -1,6 +1,5 @@
 # Rnadomizing  data, generating summary statistics, and histogram
 import numpy as np
-import matplotlib.pyplot as plt
 mu = 80
 sigma = 10
 x = np.random.normal(mu, sigma, 100)
@@ -9,7 +8,3 @@ print("Random Normal Array Centered", x[:10])
 
 print ("mean", np.mean(x))
 
-plt.hist(x)
-plt.show()
-
-print ("I am a champion of Science!")
